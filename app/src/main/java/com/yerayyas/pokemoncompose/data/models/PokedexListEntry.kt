2bd2d12a0469351642326a78032cdb8fdc27ebc7
yeray-yas <yerayarucas@gmail.com>
2023-07-16 04:40:40 +0100

    PokemonListViewModel and PokedexListEntry

---
+++ app/src/main/java/com/yerayyas/pokemoncompose/data/models/PokedexListEntry.kt
@@ -0,0 +1,7 @@
+package com.yerayyas.pokemoncompose.data.models
+
+data class PokedexListEntry(
+    val pokemonName:String,
+    val imageUrl: String,
+    val number:Int
+)
---
+++ app/src/main/java/com/yerayyas/pokemoncompose/presentation/pokemon_list/PokemonListViewModel.kt
@@ -0,0 +1,26 @@
+package com.yerayyas.pokemoncompose.presentation.pokemon_list
+
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.ViewModel
+import com.yerayyas.pokemoncompose.domain.repository.PokemonRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class PokemonListViewModel @Inject constructor(
+    private val repository: PokemonRepository
+) : ViewModel() {
+
+    fun calcDominantColor(drawable: Drawable, onFinish: (Color) -> Unit) {
+        val bmp = (drawable as BitmapDrawable).bitmap
+            .copy(Bitmap.Config.ARGB_8888, true)
+
+//        Palette.from(bmp).generate{ palette ->
+//            palette?.dominantSwatch?.rgb?.let{ colorValue ->
+//                onFinish(Color(colorValue))
+//            }
+        }
+    }
