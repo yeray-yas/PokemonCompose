9a8052989d5d68223aa3474c61a4e47c5202331d
yeray-yas <yerayarucas@gmail.com>
2023-07-16 02:08:17 +0100

    setting Dagger Hilt

--- app/src/main/java/com/yerayyas/pokemoncompose/MainActivity.kt
+++ app/src/main/java/com/yerayyas/pokemoncompose/MainActivity.kt
@@ -18,7 +18,9 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.yerayyas.pokemoncompose.ui.theme.PokemonComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/yerayyas/pokemoncompose/PokedexApplication.kt
@@ -0,0 +1,7 @@
+package com.yerayyas.pokemoncompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PokedexApplication:Application()
