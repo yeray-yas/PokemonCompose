20108a15580b50358bb4f8818b44b91d072a740d
yeray-yas <yerayarucas@gmail.com>
2023-07-16 02:20:45 +0100

    setting remote data

---
+++ app/src/main/java/com/yerayyas/pokemoncompose/data/remote/responses/GenerationViii.kt
@@ -0,0 +1,9 @@
+package com.yerayyas.pokemoncompose.data.remote.responses
+
+
+import com.google.gson.annotations.SerializedName
+
+data class GenerationViii(
+    @SerializedName("icons")
+    val icons: Icons
+)
---
+++ app/src/main/java/com/yerayyas/pokemoncompose/di/AppModule.kt
@@ -0,0 +1,18 @@
+package com.yerayyas.pokemoncompose.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provide(){
+        
+    }
+}
